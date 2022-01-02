bd0c196af0a03c01317eac3a63c1439939fd0910
tuongduy10 <vtngduy101120@gmail.com>
2022-01-02 08:59:40 +0700

    Deployt IIS

--- MusicAppAPI/Models/MusicAppContext.cs
+++ MusicAppAPI/Models/MusicAppContext.cs
@@ -31,7 +31,7 @@ namespace MusicAppAPI.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=MusicApp;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=MusicApp;User Id=sa; Password=123;Integrated Security=false;");
             }
         }
 
